b0ce945c468cc962660f8790ba36c6e25827fc9d
Your Name <you@example.com>
2024-05-21 21:13:08 +0800

    Added dependencies Laptop

--- src/main/java/com/telusko/app/Alien.java
+++ src/main/java/com/telusko/app/Alien.java
@@ -1,11 +1,13 @@
 package com.telusko.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
-
+    @Autowired
+    Laptop laptop;
     public void code(){
-        System.out.println("Coding");
+        laptop.compile();
     }
 }
