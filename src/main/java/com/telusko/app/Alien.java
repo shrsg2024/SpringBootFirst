b58b0504383e8f9dcb9768a4be35ee8caf0a5b5b
Your Name <you@example.com>
2024-05-21 21:02:01 +0800

    Added dependencies

--- src/main/java/com/telusko/app/Alien.java
+++ src/main/java/com/telusko/app/Alien.java
@@ -1,5 +1,8 @@
 package com.telusko.app;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Alien {
 
     public void code(){
--- src/main/java/com/telusko/app/SpringBootFirstApplication.java
+++ src/main/java/com/telusko/app/SpringBootFirstApplication.java
@@ -2,6 +2,7 @@ package com.telusko.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
@@ -9,6 +10,11 @@ public class SpringBootFirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstApplication.class, args);
 		System.out.println("Hello World");
+
+		ApplicationContext context = SpringApplication.run(SpringBootFirstApplication.class, args);
+
+		Alien alienObj = context.getBean(Alien.class);
+		alienObj.code();
 	}
 
 }
